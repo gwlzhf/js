bddb34c090cb3a2287edc20a02a7da030c90c430
gwlzhf <chencd520@gmail.com>
2018-02-10 09:32:49 +0800

    opencv3 gamma tunning that make dark picture brighten

---
+++ gamma.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib as mpl
+
+# 定义Gamma矫正的函数
+def gamma_trans(img, gamma):
+    # 具体做法是先归一化到1，然后gamma作为指数值求出新的像素值再还原
+    gamma_table = [np.power(x/255.0, gamma)*255.0 for x in range(256)]
+    gamma_table = np.round(np.array(gamma_table)).astype(np.uint8)
+    
+    # 实现这个映射用的是OpenCV的查表函数
+    return cv2.LUT(img, gamma_table)
+
+img = cv2.imread('/home/lhc/a.jpg')
+
+# print(img.shape)
+
+# 分通道计算每个通道的直方图
+hist_b = cv2.calcHist([img], [0], None, [256], [0, 256])
+hist_g = cv2.calcHist([img], [1], None, [256], [0, 256])
+hist_r = cv2.calcHist([img], [2], None, [256], [0, 256])
+
+# 执行Gamma矫正，小于1的值让暗部细节大量提升，同时亮部细节少量提升
+img_corrected = gamma_trans(img, 0.5)
+cv2.imwrite('/home/lhc/gamma_corrected.jpg', img_corrected)
+
+# 分通道计算Gamma矫正后的直方图
+hist_b_corrected = cv2.calcHist([img_corrected], [0], None, [256], [0, 256])
+hist_g_corrected = cv2.calcHist([img_corrected], [1], None, [256], [0, 256])
+hist_r_corrected = cv2.calcHist([img_corrected], [2], None, [256], [0, 256])
+
+# 将直方图进行可视化
+fig = plt.figure()
+
+pix_hists = [
+    [hist_b[:,0], hist_g[:,0], hist_r[:,0]],
+    [hist_b_corrected[:,0], hist_g_corrected[:,0], hist_r_corrected[:,0]]
+]
+
+pix_vals = range(256)
+for sub_plt, pix_hist in zip([121, 122], pix_hists):
+    ax = fig.add_subplot(sub_plt, projection='3d')
+    for c, z, channel_hist in zip(['b', 'g', 'r'], [20, 10, 0], pix_hist):
+        cs = [c] * 256
+        ax.bar(pix_vals, channel_hist, zs=z, zdir='y', color=cs, alpha=0.618, edgecolor='none', lw=0)
+#     ax.xaxis.set_major_locator(mpl.ticker.FixedLocator(pix_vals))
+#     ax.yaxis.set_major_locator(mpl.ticker.FixedLocator(channel_hist))
+    ax.set_xlabel('Pixel Values')
+    ax.set_xlim([0, 256])
+    ax.set_ylabel('Channels')
+    ax.set_zlabel('Counts')
+
+plt.show()
+
